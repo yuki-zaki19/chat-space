ccb8496e978ffb4c60d247844be136a37b7e7443
YukiYamazaki <yukitutuki@gmail.com>
2019-07-22 16:55:36 +0900

    edit_deploy.rb_part2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:yuki-zaki19/chat-space.git"
+set :repo_url, "git@github.com:yuki-zaki19/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
