b7b635b66755017aa03c95da01e4b670961f722f
YukiYamazaki <yukitutuki@gmail.com>
2019-07-22 16:47:13 +0900

    edit/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "chat-space"
+set :repo_url, "git@example.com:yuki-zaki19/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
